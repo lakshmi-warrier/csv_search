bccd6acd867b38587de94f8219855aba6a7e6bcf
Lakshmi Warrier <lakshmi0105warrier@gmail.com>
2021-03-14 16:37:41 +0530

    Completely removed CSV dependancy

--- csv_formatter.py
+++ csv_formatter.py
@@ -1,9 +1,7 @@
-from os import name
 import pandas as pd
 # version - 1.2.3
 
 from pathlib import Path, os
-import csv
 
 path = Path(Path.cwd(), 'Data')
 # csv files are named as 'year-orgs.csv'
@@ -12,35 +10,13 @@ starting_yr = 2018
 ending_yr = 2021
 
 
-def csv_to_json(csvFilePath):
-    jsonArray = []
-
-    # read csv file
-    with open(csvFilePath, encoding='utf-8') as csvf:
-        # load csv file data using csv library's dictionary reader
-        csvReader = csv.DictReader(csvf)
-
-        # convert each csv row into python dict
-        for row in csvReader:
-            # add this python dict to json array
-            jsonArray.append(row)
-
-    # end point of the code execution
-    return(jsonArray)
-
-
-def get_csv_path(df, tech):
-    final_file_path = Path(Path.cwd(), f"GSoC_org_list_{tech}.csv")
-    df.to_csv(final_file_path, index=False)
-    print(f"CSV file saved at {final_file_path}")
-    return(final_file_path)
-
-
 def search_by_year(year):
     filename = str(year)+'-orgs.csv'
     if os.path.exists(Path(path, filename)):
         print("File exist")
-        return(csv_to_json(Path(path, filename)))
+        df = pd.read_csv(Path(path, filename), index_col=0, header=0)
+
+        return(df.to_json(orient='records'))
     else:
         return("404")
 
@@ -55,8 +31,7 @@ def search_by_tech(tech):
         row_df = df[df['Technologies'].str.contains(tech, regex=False)]
         techwise_df = techwise_df.append(row_df)
 
-    final_file_path = get_csv_path(techwise_df, tech)
-    return(csv_to_json(final_file_path))
+    return(techwise_df.to_json(orient='records'))
     # function to return the jsonArray
 
 
@@ -67,7 +42,6 @@ def loop_thru_files():
         filename = str(year)+'-orgs.csv'
         file_path = Path(path, filename)
 
-        # search_by_year(year)
         if os.path.exists(file_path):
             existing_files.append(Path(path, filename))
 
@@ -77,17 +51,16 @@ def loop_thru_files():
 def master_fn():
     choice = input("By Tech - (1) \nby year - (2)\n")
 
-    if choice=='1':    
+    if choice == '1':
         tech = input("Enter the tech: ")
         return (search_by_tech(tech))
-    elif choice=='2':
+    elif choice == '2':
         year = input("Enter the year: ")
         return (search_by_year(year))
 
 
-#master_fn()
+# master_fn()
 print(master_fn())
-
 
 # "D:\amFOSS\hack2.0\Data\2018-orgs.csv"
 
