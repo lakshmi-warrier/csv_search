d0d2328c35b82dd113bc52070d30914aaf3b3bda
Lakshmi Warrier <lakshmi0105warrier@gmail.com>
2021-03-14 15:36:08 +0530

    Update csv_formatter.py

--- csv_formatter.py
+++ csv_formatter.py
@@ -2,7 +2,8 @@ import pandas as pd
 # version - 1.2.3
 
 from pathlib import Path, os
-
+import csv 
+import json 
 
 path = Path(Path.cwd(), 'Data')
 # csv files are named as 'year-orgs.csv'
@@ -10,10 +11,33 @@ path = Path(Path.cwd(), 'Data')
 starting_yr = 2018
 ending_yr = 2021
 
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+
+        jsonf.write(jsonString)
+
+
 def save_excel(df, tech):
     final_file_path = Path(Path.cwd(),f"GSoC_org_list_{tech}.csv")
     df.to_csv(final_file_path,index=False)
     print(f"File saved at {final_file_path}")
+    final_json_path = Path(Path.cwd(),f"GSoC_org_list_{tech}.json")
+    csv_to_json(final_file_path, final_json_path)
 
 def make_df(filepath):
     df = pd.read_csv(filepath, index_col=0)
@@ -74,6 +98,5 @@ def loop_thru_files():
 search_by_tech(input("Enter the tech: "))
 # "D:\amFOSS\hack2.0\Data\2018-orgs.csv"
 
-#TODO: search_by_year
-#TODO: search_by_tech
-# output - link of every org and its idea page as per user's wish as df
+
+# output - JSON file
